90cf4ca0afdcf85d2d5d28f1f18338202d06d7ac
johndaves6240 <johndaves6240@gmail.com>
2021-10-02 11:34:28 +0800

    deleteing .html extension

---
+++ Untitled-1.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+
+</head>
+<body>
+    <div>
+        <br>
+        <h1>Form Submission</h1>
+        <form action="" method='POST'>
+            <h3> 
+                
+                Email:&nbsp  <input type="email" name="email"><br><br>
+                Password: &nbsp <input type="text" name="name"> <br> <br>
+
+
+            </h3>
+       
+        <input id="anchor" type="submit" name="submit">
+          <!--   <a href="output.php">Collection</a> -->
+        </form>
+    </div> 
+</body>
+
+
+</html>
